e24e1dc601ef78ae86471c4ea2f1f5ad8dc88e0d
EvgenePirat <brexynckenko@sunpoint.com.ua>
2025-02-11 12:04:14 +0200

    #5.4 Created and realized crud methods in service for working with order entity

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,204 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, InternalServerErrorException, BadRequestException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { Sequelize } from 'sequelize-typescript';
+import { Order } from './entities/Order';
+import { Car } from 'src/cars/entities/Car';
+import { OrderMapper } from './mapper/OrderMapper';
+import { OrderDto } from './dto/response/OrderDto ';
+import { CreateOrderDto } from './dto/request/create-order-dto';
+import { UpdateOrderDto } from './dto/request/update-order.dto';
+import { NotEnoughCarsException } from 'src/common/exceptions/not-enougth-cars.exception';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+    constructor(
+        @InjectModel(Order) private readonly orderRepository: typeof Order,
+        @InjectModel(Car) private readonly carRepository: typeof Car,
+        private readonly sequelize: Sequelize,
+    ) { }
+
+    private calculateTotalPrice(startDate: Date, endDate: Date, hourlyRate: number, quantityCars: number): number {
+        const start = new Date(startDate);
+        const end = new Date(endDate);
+    
+        const timeDifference = end.getTime() - start.getTime();
+    
+        const hours = Math.max(1, Math.ceil(timeDifference / (1000 * 3600)));
+
+        const minutes = Math.floor((timeDifference % (1000 * 3600)) / 60000);
+        const adjustedHours = minutes > 20 ? hours + 1 : hours;
+    
+        return adjustedHours * hourlyRate * quantityCars;
+    }
+    
+
+    async getAllWithoutInclude(): Promise<OrderDto[]> {
+        const orders = await this.orderRepository.findAll();
+        return OrderMapper.orderToOrderDtoArray(orders);
+    }
+
+    async getAllWithAllInclude(): Promise<OrderDto[]> {
+        const orders = await this.orderRepository.findAll({ include: [{ model: Car }] });
+        return OrderMapper.orderToOrderDtoArray(orders);
+    }
+
+    async getOrderByIdWithAllInclude(id: number): Promise<OrderDto> {
+        const orderWithCar = await this.orderRepository.findByPk(id, { include: [{ model: Car }] });
+
+        if (!orderWithCar) {
+            throw new NotFoundException(`Order by ID ${id} not found!`);
+        }
+
+        return OrderMapper.orderToOrderDto(orderWithCar);
+    }
+
+    async getOrderByIdWithoutInclude(id: number): Promise<OrderDto> {
+        const orderWithCar = await this.orderRepository.findByPk(id);
+
+        if (!orderWithCar) {
+            throw new NotFoundException(`Order by ID ${id} not found!`);
+        }
+
+        return OrderMapper.orderToOrderDto(orderWithCar);
+    }
+
+    async create(createOrderDto: CreateOrderDto): Promise<OrderDto> {
+        const transaction = await this.sequelize.transaction();
+
+        try {
+            const car = await this.carRepository.findByPk(createOrderDto.carId, { transaction });
+
+            if (!car) {
+                throw new NotFoundException(`Car by ID ${createOrderDto.carId} not found!`);
+            }
+
+            if (car.availableQuantity < createOrderDto.quantity) {
+                throw new NotEnoughCarsException(`Not enough cars available for ID ${car.id}`);
+            }
+
+            car.availableQuantity -= createOrderDto.quantity;
+            await car.save({ transaction });
+
+            const totalPrice = this.calculateTotalPrice(
+                createOrderDto.startDate,
+                createOrderDto.endDate,
+                car.pricePerHour,
+                createOrderDto.quantity
+            );
+
+            const createdOrder = await this.orderRepository.create(
+                {
+                    ...createOrderDto,
+                    totalPrice,
+                } as Order,
+                { transaction, returning: true }
+            );
+
+            await transaction.commit();
+
+            const createdOrderWithCar = await this.getOrderByIdWithAllInclude(createdOrder.id);
+
+            return createdOrderWithCar;
+        } catch (error) {
+            await transaction.rollback();
+
+            if (error instanceof NotEnoughCarsException) {
+                throw error;
+            }
+
+            throw new InternalServerErrorException('Error creating order!');
+        }
+    }
+
+    async update(id: number, updateOrderDto: UpdateOrderDto): Promise<OrderDto> {
+        const transaction = await this.sequelize.transaction();
+    
+        try {
+            if (updateOrderDto.id && updateOrderDto.id != id) {
+                throw new BadRequestException(`Parametr ID ${id} does not match update model order ID ${updateOrderDto.id}`);
+            }
+    
+            const order = await this.orderRepository.findByPk(id, {
+                include: [{ model: Car }],
+                transaction
+            });
+    
+            if (!order) {
+                throw new NotFoundException(`Order with ID ${id} not found!`);
+            }
+    
+            if (!order.car) {
+                throw new NotFoundException(`Car by ID ${order.carId} not found!`);
+            }
+
+            const car = order.car;
+    
+            if (updateOrderDto.quantity && updateOrderDto.quantity !== order.quantity) {
+                const quantityDifference = updateOrderDto.quantity - order.quantity;
+    
+                if (quantityDifference > 0 && car.availableQuantity < quantityDifference) {
+                    throw new NotEnoughCarsException(`Not enough cars available for ID ${car.id}`);
+                }
+    
+                car.availableQuantity -= quantityDifference;
+                await car.save({ transaction });
+            }
+    
+            const totalPrice = this.calculateTotalPrice(
+                updateOrderDto.startDate || order.startDate,
+                updateOrderDto.endDate || order.endDate,
+                car.pricePerHour,
+                updateOrderDto.quantity || order.quantity
+            );
+    
+            Object.assign(order, updateOrderDto, { totalPrice });
+    
+            await order.save({ transaction });
+    
+            await transaction.commit();
+    
+            return OrderMapper.orderToOrderDto(order);
+        } catch (error) {
+            await transaction.rollback();
+    
+            if (error instanceof NotFoundException || error instanceof BadRequestException || error instanceof NotEnoughCarsException) {
+                throw error;
+            }
+    
+            throw new InternalServerErrorException('Error updating order!');
+        }
+    }
+
+    async delete(id: number): Promise<void> {
+        const transaction = await this.sequelize.transaction();
+
+        try {
+            const order = await this.orderRepository.findByPk(id, { transaction });
+
+            if (!order) {
+                throw new NotFoundException(`Order with ID ${id} not found`);
+            }
+
+            const car = await this.carRepository.findByPk(order.carId, { transaction });
+
+            if (!car) {
+                throw new NotFoundException(`Car by ID ${order.carId} not found!`);
+            }
+
+            car.availableQuantity += order.quantity;
+            await car.save({ transaction });
+
+            await order.destroy({ transaction });
+
+            await transaction.commit();
+        } catch (error) {
+            await transaction.rollback();
+
+            if (error instanceof NotFoundException) {
+                throw error;
+            }
+
+            throw new InternalServerErrorException('Error deleting order!');
+        }
+    }
+}
